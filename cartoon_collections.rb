d728dc4b3046a2249061ecd27cebd4d2e8860704
Luis Alberto Servin <lservin2015@gmail.com>
2020-02-03 16:15:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,10 @@
-array = 
-def roll_call_dwarves# code an argument here
-  # Your code here
+array = ["Doc", "Dopey", "Bashful", "Grumpy"]
+def roll_call_dwarves(array)# code an argument here
+  i = 0 
+  while i < array.length 
+  puts "#{i + 1}. #{array[i]}"
+  i += 1 
+  end# Your code here
 end
 
 def summon_captain_planet# code an argument here
