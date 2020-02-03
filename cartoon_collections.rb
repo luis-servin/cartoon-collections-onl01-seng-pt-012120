b5fd964a42b3370a7af6578407411c1ec15584e2
Luis Alberto Servin <lservin2015@gmail.com>
2020-02-03 16:24:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,16 +6,31 @@ def roll_call_dwarves(array)# code an argument here
   i += 1 
   end# Your code here
 end
-
-def summon_captain_planet# code an argument here
-  # Your code here
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+def summon_captain_planet(array)# code an argument here
+  new_array = []
+  i = 0 
+  while i = array.length 
+  new_array << array[1].capitalize + "!"
+  i += 1 
+  end 
+  new_array# Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)# code an argument here
+  i = 0 
+  if array.any? {|i| i.length > 4}
+    return true 
+    else
+      return false 
+  i += 1 
+  end# Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |type|
+    cheese_types.include?(type)
+  end
 end
